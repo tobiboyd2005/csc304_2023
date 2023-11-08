04ce227e03b6e5d9e0e43fc47c3138191da0a0a7
tobiboyd2005 <rabiaddawgz2005@gmail.com>
2023-11-08 15:24:39 +0100

    first commit

---
+++ hello_world/target/debug/deps/hello_world.d
@@ -0,0 +1,5 @@
+C:\Users\New\Documents\cs300Level\sem1\CSC305\csc304_2023\hello_world\target\debug\deps\hello_world.exe: src\main.rs
+
+C:\Users\New\Documents\cs300Level\sem1\CSC305\csc304_2023\hello_world\target\debug\deps\hello_world.d: src\main.rs
+
+src\main.rs:
